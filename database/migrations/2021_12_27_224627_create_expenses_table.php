ef9fd851628388344660b74eb1a09441086de8d3
matheusdepaula <matheus@nvgo.com.br>
2021-12-27 20:01:23 -0300

    iniciando projeto

---
+++ database/migrations/2021_12_27_224627_create_expenses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('description');
+            $table->integer('type')->coment("1 - Entrada | 2 - Saída");
+            $table->integer('amount');// 29.90 ->2990
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
